7450095369231ff491cf99efe22ea42d51d35611
yuler <is.yuler@gmail.com>
2025-09-18 16:42:35 +0800

    Setup cable

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
+  # Action Cable WebSocket endpoint
+  mount ActionCable.server => '/cable'
+
   # Defines the root path route ("/")
   root "quotes#index"
   resources :quotes
