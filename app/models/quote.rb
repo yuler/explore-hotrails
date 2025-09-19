4e782df3abd43525373aea14b7e260f82fc42e12
yuler <is.yuler@gmail.com>
2025-09-19 13:50:15 +0800

    Quote CURD

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,6 @@ class Quote < ApplicationRecord
     validates :name, presence: true
 
     default_scope { order(created_at: :desc) }
+
+    broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_16_085153) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_19_054740) do
+  create_table "companies", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "quotes", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
